7d2b65ea8938a538e18347b2c7acb2a2bc0942eb
Züleyha <yetkinzuleyha94@gmail.com>
2024-08-09 12:21:41 +0300

    first

--- models/model.go
+++ models/model.go
@@ -17,36 +17,3 @@ type Product struct {
 	Stock       int     `json:"stock" gorm:"not null"`
 }
 
-// GetAllProducts tüm ürünleri döndürür
-func GetAllProducts() ([]Product, error) {
-	var products []Product
-	if err := config.DB.Find(&products).Error; err != nil {
-		return nil, err
-	}
-	return products, nil
-}
-
-// GetProductByID belirli bir ID'ye sahip ürünü döndürür
-func GetProductByID(id string) (*Product, error) {
-	var product Product
-
-	if err := config.DB.First(&product, id).Error; err != nil {
-		return nil, err
-	}
-	return &product, nil
-}
-
-// CreateProduct yeni bir ürün oluştur
-func CreateProduct(product *Product) error {
-	return config.DB.Create(product).Error
-}
-
-
-func UpdateProduct(id string, product *Product) error {
-	return config.DB.Model(&Product{}).Where("id = ?", id).Updates(product).Error
-}
-
-// DeleteProduct belirli bir ID'ye sahip ürünü siler
-func DeleteProduct(id string) error {
-	return config.DB.Delete(&Product{}, id).Error
-}
